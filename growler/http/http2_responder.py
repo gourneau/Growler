44803e3d714e5f8921b0df5e0a3c57298a5e7004
Andrew Kubera <andrewkubera@gmail.com>
2015-04-25 21:34:14 +0200

    Added a (silly) switch_protocol method to http_2

--- growler/http/http2_responder.py
+++ growler/http/http2_responder.py
@@ -12,6 +12,7 @@ class GrowlerHTTP2Responder:
     from the 'standard' responder upon an upgrade header from the client. The
     implementation still needs to be done.
     """
+    PREFACE = 0x505249202a20485454502f322e300d0a0d0a534d0d0a0d0a
 
     def __init__(self, protocol):
         """
@@ -25,3 +26,13 @@ class GrowlerHTTP2Responder:
         The incoming data function for responders.
         """
         pass
+
+    @classmethod
+    def switch_protocols(cls, req, res):
+        switch = '''HTTP/1.1 101 Switching Protocols
+Connection: Upgrade
+Upgrade: h2c
+
+'''
+        res.status = 101
+        res.write(switch)
